50c8552aa036053128c80f63d744125c17920d9e
Aaron Trent <novacrazy@gmail.com>
2017-04-25 14:30:42 -0500

    Add constructor and clear method for framebuffer

--- src/render/framebuffer.rs
+++ src/render/framebuffer.rs
@@ -1,4 +1,5 @@
 //! Minimalist framebuffer structure with an emphasis on performance
+use rayon;
 
 use ::pixel::Pixel;
 
@@ -14,7 +15,44 @@ pub struct FrameBuffer<P: Pixel> {
     viewport: (f32, f32),
 }
 
+pub const DEFAULT_DEPTH_VALUE: f32 = 1000000.0;
+
 impl<P: Pixel> FrameBuffer<P> {
+    /// Create a new framebuffer with the default pixel.
+    pub fn new(width: u32, height: u32) -> FrameBuffer<P> where P: Default {
+        FrameBuffer::new_with(width, height, P::default())
+    }
+
+    /// Creates a new framebuffer with the given pixel.
+    pub fn new_with(width: u32, height: u32, pixel: P) -> FrameBuffer<P> {
+        let len = width as usize * height as usize;
+        FrameBuffer {
+            width: width,
+            height: height,
+            depth: vec![DEFAULT_DEPTH_VALUE; len],
+            color: vec![pixel; len],
+            blend_func: Box::new(|s, _| s),
+            viewport: (width as f32, height as f32)
+        }
+    }
+
+    /// Sets all depth and color values to their default
+    pub fn clear(&mut self) where P: Default {
+        self.clear_with(P::default())
+    }
+
+    /// Sets all depth values to their default and the color values to the given pixel.
+    pub fn clear_with(&mut self, pixel: P) {
+        let ref mut depth = self.depth;
+        let ref mut color = self.color;
+
+        // Might as well clear them both in parallel.
+        // Although I'll have to test if it's faster this way or to just do them sequentially,
+        // because of thread scheduling overhead and so forth.
+        rayon::join(|| { for mut dv in depth { *dv = DEFAULT_DEPTH_VALUE; } },
+                    || { for mut pv in color { *pv = pixel; } });
+    }
+
     /// Get the projection viewport dimensions
     #[inline(always)]
     pub fn viewport(&self) -> (f32, f32) { self.viewport }
@@ -55,7 +93,7 @@ impl<P: Pixel> FrameBuffer<P> {
     /// so bounds should be checked elsewhere.
     #[inline]
     pub unsafe fn pixel(&self, x: u32, y: u32) -> &P {
-        self.color.get_unchecked((x + y * self.width) as usize)
+        self.color.get_unchecked(x as usize + y as usize * self.width as usize)
     }
 
     /// Get a mutable reference to the pixel at the given coordinate.
@@ -64,7 +102,7 @@ impl<P: Pixel> FrameBuffer<P> {
     /// so bounds should be checked elsewhere.
     #[inline]
     pub unsafe fn pixel_mut(&mut self, x: u32, y: u32) -> &mut P {
-        self.color.get_unchecked_mut((x + y * self.width) as usize)
+        self.color.get_unchecked_mut(x as usize + y as usize * self.width as usize)
     }
 
     /// Get a reference to the depth value at the given coordinate.
@@ -73,7 +111,7 @@ impl<P: Pixel> FrameBuffer<P> {
     /// so bounds should be checked elsewhere.
     #[inline]
     pub unsafe fn depth(&self, x: u32, y: u32) -> &f32 {
-        self.depth.get_unchecked((x + y * self.width) as usize)
+        self.depth.get_unchecked(x as usize + y as usize * self.width as usize)
     }
 
     /// Get a mutable reference to the depth value at the given coordinate.
@@ -82,6 +120,6 @@ impl<P: Pixel> FrameBuffer<P> {
     /// so bounds should be checked elsewhere.
     #[inline]
     pub unsafe fn depth_mut(&mut self, x: u32, y: u32) -> &mut f32 {
-        self.depth.get_unchecked_mut((x + y * self.width) as usize)
+        self.depth.get_unchecked_mut(x as usize + y as usize * self.width as usize)
     }
 }
