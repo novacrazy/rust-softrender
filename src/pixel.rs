6b52dfb97d8a427bcb31e1aa15b22e7863ef99ad
Aaron Trent <novacrazy@gmail.com>
2017-04-25 14:59:22 -0500

    Add Copy + Clone to Pixel trait and implement Default for RGBAf32Pixel

--- src/pixel.rs
+++ src/pixel.rs
@@ -1,7 +1,7 @@
 //! Pixel definition and operations
 
 /// Trait required to distinguish pixel type for use in the framebuffer and fragment shader
-pub trait Pixel: Send + Sync {}
+pub trait Pixel: Clone + Copy + Send + Sync {}
 
 /// The most dead simple f32 pixel representation
 #[derive(Debug, Clone, Copy)]
@@ -15,6 +15,12 @@ pub struct RGBAf32Pixel {
     pub b: f32,
     /// Alpha Component
     pub a: f32,
+}
+
+impl Default for RGBAf32Pixel {
+    fn default() -> RGBAf32Pixel {
+        RGBAf32Pixel { r: 0.0, g: 0.0, b: 0.0, a: 1.0 }
+    }
 }
 
 impl Pixel for RGBAf32Pixel {}
