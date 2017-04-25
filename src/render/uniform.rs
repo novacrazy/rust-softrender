da58636f02c36222111b5b01e931ab6fa654eba1
Aaron Trent <novacrazy@gmail.com>
2017-04-25 13:48:12 -0500

    Simplify BarycentricInterpolation trait requirements

--- src/render/uniform.rs
+++ src/render/uniform.rs
@@ -7,7 +7,13 @@ use std::ops::{Add, Mul};
 /// Describes a type that can be interpolated with barycentric coordinates.
 ///
 /// This is required for any rasterization to occur.
-pub trait BarycentricInterpolation: Sized + Add<Output=Self> + Add<f32, Output=Self> + Mul<f32, Output=Self> {
+///
+/// See [This document](https://classes.soe.ucsc.edu/cmps160/Fall10/resources/barycentricInterpolation.pdf) for more information
+pub trait BarycentricInterpolation {
+    fn interpolate(a: f32, a1: f32, x1: Self, a2: f32, x2: Self, a3: f32, x3: Self) -> Self;
+}
+
+impl<T> BarycentricInterpolation for T where T: Add<Output=Self> + Add<f32, Output=Self> + Mul<f32, Output=Self> {
     fn interpolate(a: f32, a1: f32, x1: Self, a2: f32, x2: Self, a3: f32, x3: Self) -> Self {
         // In debug mode, assert that the total combined area is approximately equal to the actual area
         debug_assert!(((a1 + a2 + a3) - a).abs() <= 0.1);
@@ -15,5 +21,3 @@ pub trait BarycentricInterpolation: Sized + Add<Output=Self> + Add<f32, Output=S
         (x1 * a1 + x2 * a2 + x3 * a3) * (1.0 / a)
     }
 }
-
-impl<T> BarycentricInterpolation for T where T: Sized + Add<Output=Self> + Add<f32, Output=Self> + Mul<f32, Output=Self> {}
