147e6aeebc0f9a76393471c1b85d01f7a5902921
Aaron Trent <novacrazy@gmail.com>
2017-04-26 02:50:07 -0500

    Add "empty" Pixel required method

--- src/pixel.rs
+++ src/pixel.rs
@@ -1,7 +1,9 @@
 //! Pixel definition and operations
 
 /// Trait required to distinguish pixel type for use in the framebuffer and fragment shader
-pub trait Pixel: Clone + Copy + Send + Sync {}
+pub trait Pixel: Clone + Copy + Send + Sync {
+    fn empty() -> Self;
+}
 
 /// The most dead simple f32 pixel representation
 #[derive(Debug, Clone, Copy)]
@@ -23,4 +25,8 @@ impl Default for RGBAf32Pixel {
     }
 }
 
-impl Pixel for RGBAf32Pixel {}
+impl Pixel for RGBAf32Pixel {
+    fn empty() -> RGBAf32Pixel {
+        RGBAf32Pixel { r: 0.0, g: 0.0, b: 0.0, a: 0.0 }
+    }
+}
