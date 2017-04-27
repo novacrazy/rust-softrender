4319e5d715a194cc02269cd81c8b69632d0ede96
Aaron Trent <novacrazy@gmail.com>
2017-04-27 16:30:55 -0500

    Add suzanne mesh for testing with normals

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,8 +1,10 @@
 extern crate nalgebra;
+extern crate tobj;
 #[macro_use]
 extern crate softrender;
 
 use std::sync::Arc;
+use std::path::Path;
 
 use nalgebra::{Point3, Vector4, Vector3};
 
@@ -11,59 +13,37 @@ use softrender::mesh::{Mesh, Vertex};
 use softrender::render::{FrameBuffer, Pipeline, ClipVertex, ScreenVertex};
 use softrender::image_compat::ImageFrameBuffer;
 
-fn get_cube() -> Arc<Mesh<()>> {
-    let vertices = vec![Point3::new(0.583, 0.771, 0.014),
-                        Point3::new(0.609, 0.115, 0.436),
-                        Point3::new(0.327, 0.483, 0.844),
-                        Point3::new(0.822, 0.569, 0.201),
-                        Point3::new(0.435, 0.602, 0.223),
-                        Point3::new(0.310, 0.747, 0.185),
-                        Point3::new(0.597, 0.770, 0.761),
-                        Point3::new(0.559, 0.436, 0.730),
-                        Point3::new(0.359, 0.583, 0.152),
-                        Point3::new(0.483, 0.596, 0.789),
-                        Point3::new(0.559, 0.861, 0.639),
-                        Point3::new(0.195, 0.548, 0.859),
-                        Point3::new(0.014, 0.184, 0.576),
-                        Point3::new(0.771, 0.328, 0.970),
-                        Point3::new(0.406, 0.615, 0.116),
-                        Point3::new(0.676, 0.977, 0.133),
-                        Point3::new(0.971, 0.572, 0.833),
-                        Point3::new(0.140, 0.616, 0.489),
-                        Point3::new(0.997, 0.513, 0.064),
-                        Point3::new(0.945, 0.719, 0.592),
-                        Point3::new(0.543, 0.021, 0.978),
-                        Point3::new(0.279, 0.317, 0.505),
-                        Point3::new(0.167, 0.620, 0.077),
-                        Point3::new(0.347, 0.857, 0.137),
-                        Point3::new(0.055, 0.953, 0.042),
-                        Point3::new(0.714, 0.505, 0.345),
-                        Point3::new(0.783, 0.290, 0.734),
-                        Point3::new(0.722, 0.645, 0.174),
-                        Point3::new(0.302, 0.455, 0.848),
-                        Point3::new(0.225, 0.587, 0.040),
-                        Point3::new(0.517, 0.713, 0.338),
-                        Point3::new(0.053, 0.959, 0.120),
-                        Point3::new(0.393, 0.621, 0.362),
-                        Point3::new(0.673, 0.211, 0.457),
-                        Point3::new(0.820, 0.883, 0.371),
-                        Point3::new(0.982, 0.099, 0.879)];
+struct VertexData {
+    normal: Vector3<f32>,
+}
 
-    let indices = (0..vertices.len()).map(|i| i as u32).collect();
+fn get_mesh() -> Arc<Mesh<VertexData>> {
+    let (models, _): (Vec<tobj::Model>, _) = tobj::load_obj(Path::new("examples/suzanne.obj")).unwrap();
+
+    let ref mesh: tobj::Mesh = models[0].mesh;
+
+    assert_eq!(mesh.positions.len(), mesh.normals.len());
 
     Arc::new(Mesh {
-        vertices: vertices.into_iter().map(|vertex| Vertex { position: vertex, vertex_data: () }).collect(),
-        indices: indices
+        vertices: mesh.positions.chunks(3).zip(mesh.normals.chunks(3)).map(|(position, normal)| {
+            Vertex {
+                position: Point3::new(position[0], position[1], position[2]),
+                vertex_data: VertexData {
+                    normal: Vector3::new(normal[0], normal[1], normal[2]),
+                }
+            }
+        }).collect(),
+        indices: mesh.indices.clone()
     })
 }
 
 fn main() {
     let framebuffer = FrameBuffer::<RGBAf32Pixel>::new_with(1000, 1000, RGBAf32Pixel { r: 1.0, g: 1.0, b: 1.0, a: 1.0 });
 
-    let cube = get_cube();
+    let mesh = get_mesh();
 
     let view = nalgebra::Isometry3::look_at_rh(
-        &Point3::new(1.0, 1.0, 1.0),
+        &Point3::new(1.0, 0.0, 3.0),
         &Point3::origin(),
         &Vector3::new(0.0, 1.0, 0.0)
     ).to_homogeneous();
@@ -74,21 +54,25 @@ fn main() {
     let mut pipeline = Pipeline::new(framebuffer, ());
 
     {
-        let vertex_shader = pipeline.render_mesh(cube.clone());
+        let vertex_shader = pipeline.render_mesh(mesh.clone());
 
         declare_uniforms! {
             Uniforms {
                 position: Vector4<f32>,
+                normal: Vector4<f32>,
             }
         }
 
         let mut fragment_shader = vertex_shader.run(|vertex, _| {
+            let VertexData { normal } = vertex.vertex_data;
+
             let world_position = vertex.position.to_homogeneous();
 
-            let position = projection * view * world_position;
+            let clip_position = projection * view * world_position;
 
-            ClipVertex::new(position, Uniforms {
+            ClipVertex::new(clip_position, Uniforms {
                 position: world_position,
+                normal: normal.to_homogeneous(),
             })
         });
 
@@ -104,11 +88,17 @@ fn main() {
             }
         });
 
-        fragment_shader.run(|screen_vertex, _| {
-            let Uniforms { position } = screen_vertex.uniforms;
+        fragment_shader.triangles(|screen_vertex, _| {
+            let Uniforms { position, normal } = screen_vertex.uniforms;
+
+            let light = Point3::new(1.0, 1.0, 1.0).to_homogeneous();
+
+            let light_dir = (light - position).normalize();
+
+            let cos_theta = light_dir.dot(&normal);
 
             // Determine the color of the pixel here
-            RGBAf32Pixel { r: position.x, g: position.y, b: position.z, a: 1.0 }
+            RGBAf32Pixel { r: 0.0, g: cos_theta, b: 0.0, a: 1.0 }
         });
     }
 
