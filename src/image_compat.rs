e96d0bde473f38e86208a87e40f0a4537d03a3d4
Aaron Trent <novacrazy@gmail.com>
2017-04-27 05:12:36 -0500

    Clamp RGBAf32Pixel values before converting them to u8.

--- src/image_compat.rs
+++ src/image_compat.rs
@@ -14,10 +14,10 @@ impl ImageFrameBuffer<image::Rgba<u8>, Vec<u8>> for FrameBuffer<RGBAf32Pixel> {
         let mut res = Vec::with_capacity(color_buffer.len() * 4);
 
         for color in color_buffer {
-            res.push((color.r * 255.0) as u8);
-            res.push((color.g * 255.0) as u8);
-            res.push((color.b * 255.0) as u8);
-            res.push((color.a * 255.0) as u8);
+            res.push((color.r.max(0.0).min(1.0) * 255.0).floor() as u8);
+            res.push((color.g.max(0.0).min(1.0) * 255.0).floor() as u8);
+            res.push((color.b.max(0.0).min(1.0) * 255.0).floor() as u8);
+            res.push((color.a.max(0.0).min(1.0) * 255.0).floor() as u8);
         }
 
         image::RgbaImage::from_raw(self.width(), self.height(), res)
