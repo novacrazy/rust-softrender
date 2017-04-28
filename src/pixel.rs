5402f72c080eb3caec68aaac11c1ebb3afa6acd3
Aaron Trent <novacrazy@gmail.com>
2017-04-28 17:16:16 -0500

    Add with_alpha method to pixels to scale alpha values

--- src/pixel.rs
+++ src/pixel.rs
@@ -3,6 +3,7 @@
 /// Trait required to distinguish pixel type for use in the framebuffer and fragment shader
 pub trait Pixel: Clone + Copy + Send + Sync {
     fn empty() -> Self;
+    fn with_alpha(self, alpha: f32) -> Self;
 }
 
 /// The most dead simple f32 pixel representation
@@ -28,5 +29,9 @@ impl Default for RGBAf32Pixel {
 impl Pixel for RGBAf32Pixel {
     fn empty() -> RGBAf32Pixel {
         RGBAf32Pixel { r: 0.0, g: 0.0, b: 0.0, a: 0.0 }
+    }
+
+    fn with_alpha(self, alpha: f32) -> RGBAf32Pixel {
+        RGBAf32Pixel { r: self.r, g: self.g, b: self.b, a: self.a * alpha }
     }
 }
