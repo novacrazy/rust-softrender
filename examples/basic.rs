6d2d33bc25c2fed49c20517e9a4b57ab6090c9e8
Aaron Trent <novacrazy@gmail.com>
2017-04-27 21:05:35 -0500

    Update example with blinn-phong specular highlights, more uniform examples and gamma correction

--- examples/basic.rs
+++ examples/basic.rs
@@ -6,19 +6,19 @@ extern crate softrender;
 use std::sync::Arc;
 use std::path::Path;
 
-use nalgebra::{Point3, Vector4, Vector3};
+use nalgebra::{Point3, Vector4, Vector3, Matrix4};
 
 use softrender::pixel::RGBAf32Pixel;
 use softrender::mesh::{Mesh, Vertex};
-use softrender::render::{FrameBuffer, Pipeline, ClipVertex, ScreenVertex};
+use softrender::render::{FaceWinding, FrameBuffer, Pipeline, ClipVertex, ScreenVertex};
 use softrender::image_compat::ImageFrameBuffer;
 
 struct VertexData {
     normal: Vector3<f32>,
 }
 
-fn get_mesh() -> Arc<Mesh<VertexData>> {
-    let (models, _): (Vec<tobj::Model>, _) = tobj::load_obj(Path::new("examples/suzanne.obj")).unwrap();
+fn load_mesh<P: AsRef<Path>>(p: P) -> Arc<Mesh<VertexData>> {
+    let (models, _): (Vec<tobj::Model>, _) = tobj::load_obj(p.as_ref().clone()).unwrap();
 
     let ref mesh: tobj::Mesh = models[0].mesh;
 
@@ -37,42 +37,69 @@ fn get_mesh() -> Arc<Mesh<VertexData>> {
     })
 }
 
-fn main() {
-    let framebuffer = FrameBuffer::<RGBAf32Pixel>::new_with(1000, 1000, RGBAf32Pixel { r: 1.0, g: 1.0, b: 1.0, a: 1.0 });
+fn get_meshes() -> Vec<Arc<Mesh<VertexData>>> {
+    vec![
+        load_mesh("examples/suzanne_highres.obj"),
+        load_mesh("examples/plane.obj")
+    ]
+}
 
-    let mesh = get_mesh();
+#[allow(non_snake_case)]
+fn main() {
+    let framebuffer = FrameBuffer::<RGBAf32Pixel>::new_with(2000, 2000, RGBAf32Pixel { r: 0.01, g: 0.01, b: 0.01, a: 1.0 });
+
+    struct GlobalUniforms {
+        camera: Vector4<f32>,
+        model: Matrix4<f32>,
+        model_inverse_transpose: Matrix4<f32>,
+        view: Matrix4<f32>,
+        projection: Matrix4<f32>,
+    }
+
+    declare_uniforms! {
+        Uniforms {
+            position: Vector4<f32>,
+            normal: Vector4<f32>,
+        }
+    }
+
+    let model = nalgebra::Isometry3::new(Vector3::new(0.0, 0.0, 0.0),
+                                         Vector3::new(0.0, 0.0, 0.0));
+
+    let camera_position = Point3::new(2.0, 2.0, 2.0);
 
     let view = nalgebra::Isometry3::look_at_rh(
-        &Point3::new(1.0, 0.0, 3.0),
-        &Point3::origin(),
+        &camera_position,
+        &Point3::new(0.0, 0.0, 0.0),
         &Vector3::new(0.0, 1.0, 0.0)
     ).to_homogeneous();
 
     let projection = nalgebra::Perspective3::new(framebuffer.width() as f32 / framebuffer.height() as f32,
                                                  75.0f32.to_radians(), 0.001, 1000.0).to_homogeneous();
 
-    let mut pipeline = Pipeline::new(framebuffer, ());
+    let mut pipeline = Pipeline::new(framebuffer, GlobalUniforms {
+        camera: camera_position.to_homogeneous(),
+        model: model.to_homogeneous(),
+        model_inverse_transpose: model.inverse().to_homogeneous().transpose(),
+        view: view,
+        projection: projection
+    });
 
-    {
+    for mesh in &get_meshes() {
         let vertex_shader = pipeline.render_mesh(mesh.clone());
 
-        declare_uniforms! {
-            Uniforms {
-                position: Vector4<f32>,
-                normal: Vector4<f32>,
-            }
-        }
-
-        let mut fragment_shader = vertex_shader.run(|vertex, _| {
+        let mut fragment_shader = vertex_shader.run(|vertex, global_uniforms| {
+            let GlobalUniforms { ref view, ref projection, ref model, ref model_inverse_transpose, .. } = *global_uniforms;
             let VertexData { normal } = vertex.vertex_data;
 
             let world_position = vertex.position.to_homogeneous();
+            let normal = (model_inverse_transpose * normal.to_homogeneous()).normalize();
 
             let clip_position = projection * view * world_position;
 
             ClipVertex::new(clip_position, Uniforms {
                 position: world_position,
-                normal: normal.to_homogeneous(),
+                normal: normal,
             })
         });
 
@@ -88,19 +115,68 @@ fn main() {
             }
         });
 
-        fragment_shader.triangles(|screen_vertex, _| {
+        let light_scale = 1.0;
+
+        let lights = [
+            Point3::new(light_scale * 1.0, light_scale * 1.0, light_scale * 1.0),
+            Point3::new(light_scale * -1.0, light_scale * 1.0, light_scale * -1.0),
+            Point3::new(light_scale * 0.0, light_scale * 3.0, light_scale * -1.0),
+            Point3::new(light_scale * -2.0, light_scale * -1.0, light_scale * 1.0),
+        ];
+
+        let diffuse_color = RGBAf32Pixel { r: 0.1, g: 0.1, b: 0.6, a: 1.0 };
+
+        fragment_shader.triangles(|screen_vertex, global_uniforms| {
+            let GlobalUniforms { ref camera, .. } = *global_uniforms;
+            let frag_coord = screen_vertex.position;
             let Uniforms { position, normal } = screen_vertex.uniforms;
 
-            let light = Point3::new(1.0, 1.0, 1.0).to_homogeneous();
+            let view_dir = (camera - position).normalize();
+            let NdotV = normal.dot(&view_dir).min(1.0).max(0.0);
 
-            let light_dir = (light - position).normalize();
+            let mut specular = 0.0;
+            let mut diffuse = 0.0;
 
-            let cos_theta = light_dir.dot(&normal);
+            for light in &lights {
+                let light = light.to_homogeneous();
 
-            // Determine the color of the pixel here
-            RGBAf32Pixel { r: 0.0, g: cos_theta, b: 0.0, a: 1.0 }
+                let light_dir = (light - position).normalize();
+                let halfway_vector = (light_dir + view_dir).normalize();
+
+                let NdotL = light_dir.dot(&normal).min(1.0).max(0.0);
+                let NdotH = normal.dot(&halfway_vector).min(1.0).max(0.0);
+                let VdotH = view_dir.dot(&halfway_vector).min(1.0).max(0.0);
+
+                fn fresnel_schlick(cos_theta: f32, ior: f32) -> f32 {
+                    let f0 = ((1.0 - ior) / (1.0 + ior)).powi(2);
+
+                    f0 + (1.0 - f0) * (1.0 - cos_theta).powi(5)
+                }
+
+                let f = fresnel_schlick(VdotH, 1.45);
+
+                diffuse += NdotL * (1.0 - f);
+                specular += f * NdotH.powf(24.0);
+            }
+
+            RGBAf32Pixel {
+                r: specular * 10.0 + (diffuse * diffuse_color.r),
+                g: specular * 10.0 + (diffuse * diffuse_color.g),
+                b: specular * 10.0 + (diffuse * diffuse_color.b),
+                a: diffuse_color.a
+            }
         });
     }
+
+    println!("Adjusting gamma");
+
+    for pixel in pipeline.framebuffer_mut().color_buffer_mut().iter_mut() {
+        pixel.r = pixel.r.powf(1.0 / 2.2);
+        pixel.g = pixel.g.powf(1.0 / 2.2);
+        pixel.b = pixel.b.powf(1.0 / 2.2);
+    }
+
+    println!("Saving to image");
 
     let image = pipeline.framebuffer().copy_to_image().unwrap();
 
