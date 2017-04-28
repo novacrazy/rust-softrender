e0ea35d7845930cc431e4772ab8e15ace058def4
Aaron Trent <novacrazy@gmail.com>
2017-04-27 21:04:53 -0500

    Remove accidentally copied documentation from another project

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,4 @@
 /// Clamp a value to the given range
-///
-/// ```
-/// use combustion_common::num_utils::clamp;
-///
-/// assert_eq!(clamp(15u32, 0, 5), 5);
-/// ```
 pub fn clamp<T>(value: T, min: T, max: T) -> T where T: PartialOrd {
     if value < min { min } else if value > max { max } else { value }
 }
