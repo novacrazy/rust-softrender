76298851221882272c592d3529df739889a0194e
Aaron Trent <novacrazy@gmail.com>
2017-04-30 22:26:06 -0500

    Improve shader documentation

--- full_example/src/shaders.rs
+++ full_example/src/shaders.rs
@@ -9,10 +9,13 @@ pub fn vertex_shader(vertex: &Vertex<VertexData>, global_uniforms: &GlobalUnifor
     let GlobalUniforms { ref view, ref projection, ref model, ref model_inverse_transpose, .. } = *global_uniforms;
     let VertexData { normal, uv } = vertex.vertex_data;
 
+    // Transform vertex position to world-space
     let world_position = model * vertex.position.to_homogeneous();
 
+    // Transform normal to world-space
     let normal = (model_inverse_transpose * normal.to_homogeneous()).normalize();
 
+    // Transform vertex position to clip-space (projection-space)
     let clip_position = projection * view * world_position;
 
     // Return the clip-space position and any uniforms to interpolate and pass into the fragment shader
@@ -42,19 +45,29 @@ pub fn fragment_shader(vertex: &ScreenVertex<Uniforms>, global_uniforms: &Global
 
     let view_dir = (camera - position).normalize();
 
-    let shininess = 64.0;
+    // Specular "shininess", almost like a roughness parameter but not really.
+    let shininess = 32.0;
 
+    // Dark grey surface
     let material_color = decode_gamma(Color { r: 0.25, g: 0.25, b: 0.25, a: 1.0 }, SRGB_GAMMA);
 
+    // Surface albedo, or how bright diffuse lighting is
     let albedo = 0.7;
 
+    // Start with a black fragment to accumulate lighting
     let mut color: Color = Color { r: 0.0, g: 0.0, b: 0.0, a: 1.0 };
 
     for light in lights {
         let light_position = light.position.to_homogeneous();
 
+        // Get distance to light for simple light attenuation
+        let light_distance = (light_position - position).norm();
+
         let light_dir = (light_position - position).normalize();
         let halfway_vector = (light_dir + view_dir).normalize();
+
+        // Calculate light attenuation based on inverse distance squared
+        let intensity = light.intensity / light_distance.powi(2);
 
         let NdotL = saturate(light_dir.dot(&normal));
         let NdotH = saturate(normal.dot(&halfway_vector));
@@ -69,13 +82,14 @@ pub fn fragment_shader(vertex: &ScreenVertex<Uniforms>, global_uniforms: &Global
         // simple Blinn-Phong specular
         let specular = f * NdotH.powf(shininess * 2.0);
 
-        color = Color {
-            r: color.r + light.intensity * light.color.r * (specular + (diffuse * albedo * material_color.r)),
-            g: color.g + light.intensity * light.color.g * (specular + (diffuse * albedo * material_color.g)),
-            b: color.b + light.intensity * light.color.b * (specular + (diffuse * albedo * material_color.b)),
-            a: color.a,
-        };
+        // Add specular and diffuse colors together, multiple by light color,
+        // and multiple by light intensity accounting for distance attenuation,
+        // then add it to the previous color
+        color.r += intensity * light.color.r * (specular + (diffuse * albedo * material_color.r));
+        color.g += intensity * light.color.g * (specular + (diffuse * albedo * material_color.g));
+        color.b += intensity * light.color.b * (specular + (diffuse * albedo * material_color.b));
     }
 
+    // Tonemap and encode gamma for fragment
     Fragment::Color(encode_gamma(aces_filmic_tonemap(color), SRGB_GAMMA))
 }
