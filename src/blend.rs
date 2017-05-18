46ad18315b8f90323607d382a73168735f95170b
Aaron Trent <novacrazy@gmail.com>
2017-05-17 21:44:58 -0500

    Simplify Blend trait and add generic constant-function blend operator as opposed to boxing the functions

--- src/blend.rs
+++ src/blend.rs
@@ -15,78 +15,53 @@ pub trait Blend<P: Pixel>: Send + Sync {
     ///
     /// For a generic alpha-over blend function, check the Wikipedia article [Here](https://en.wikipedia.org/wiki/Alpha_compositing)
     /// for the *over* color function.
-    fn blend(&self, P, P) -> P;
-
-    #[inline]
-    fn blend_by_depth(&self, foreground_depth: f32, background_depth: f32, a: P, b: P) -> P {
-        if background_depth > foreground_depth {
-            self.blend(a, b)
-        } else {
-            self.blend(b, a)
-        }
-    }
-
-    fn ignore_depth(&self) -> bool;
+    fn blend(&self, a: P, b: P) -> P;
 }
 
 impl<P: Pixel> Blend<P> for () {
     #[inline(always)]
     fn blend(&self, a: P, _: P) -> P { a }
-
-    #[inline(always)]
-    fn ignore_depth(&self) -> bool { false }
 }
 
 /// Generic blend structure that can accept a user-defined blend function
 #[derive(Clone)]
-pub struct GenericBlend<P: Pixel> {
+pub struct BoxedGenericBlend<P: Pixel> {
     blend_func: Arc<Box<Fn(P, P) -> P + Send + Sync>>,
-    ignore_depth: bool,
 }
 
-impl<P: Pixel> GenericBlend<P> {
+impl<P: Pixel> BoxedGenericBlend<P> {
     pub fn set_blend_function<F>(&mut self, f: F) where F: Fn(P, P) -> P + Send + Sync + 'static {
         self.blend_func = Arc::new(Box::new(f))
     }
-
-    pub fn ignore_depth(&mut self, enable: bool) {
-        self.ignore_depth = enable;
-    }
 }
 
-impl<P: Pixel> Default for GenericBlend<P> {
-    fn default() -> GenericBlend<P> {
-        GenericBlend {
+impl<P: Pixel> Default for BoxedGenericBlend<P> {
+    fn default() -> BoxedGenericBlend<P> {
+        BoxedGenericBlend {
             blend_func: Arc::new(Box::new(|a, _| a)),
-            ignore_depth: false
         }
     }
 }
 
-impl<P: Pixel> Blend<P> for GenericBlend<P> {
-    #[inline(always)]
+impl<P: Pixel> Blend<P> for BoxedGenericBlend<P> {
     fn blend(&self, a: P, b: P) -> P {
         (**self.blend_func)(a, b)
     }
-
-    #[inline(always)]
-    fn ignore_depth(&self) -> bool { self.ignore_depth }
 }
 
-/*
-#[derive(Debug)]
-pub struct BlendOver<P: Pixel>(PhantomData<P>);
-
-impl<P: Pixel> Clone for BlendOver<P> {
-    #[inline(always)]
-    fn clone(&self) -> BlendOver<P> { BlendOver(PhantomData) }
+pub struct GenericBlend<P: Pixel, F> {
+    blend_func: F,
+    pixel: PhantomData<P>,
 }
 
-impl<P: Pixel> Copy for BlendOver<P> {}
-
-impl<P: Pixel> Blend<P> for BlendOver<P> {
-    fn blend(&self, a: P, b: P) -> P {
-        //TODO
+impl<P: Pixel, F> GenericBlend<P, F> {
+    pub fn new(blend_func: F) -> GenericBlend<P, F> where F: Fn(P, P) -> P + Send + Sync + 'static  {
+        GenericBlend { blend_func, pixel: PhantomData }
     }
 }
-*/
+
+impl<P: Pixel, F> Blend<P> for GenericBlend<P, F> where F: Fn(P, P) -> P + Send + Sync + 'static {
+    fn blend(&self, a: P, b: P) -> P {
+        (self.blend_func)(a, b)
+    }
+}
