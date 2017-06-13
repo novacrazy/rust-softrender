7358ec6f4cb42a84634abb00020f3f67e208c8ba
Aaron Trent <novacrazy@gmail.com>
2017-06-13 05:56:43 -0500

    Mark num_vertices as inline(always)

--- src/primitive.rs
+++ src/primitive.rs
@@ -64,6 +64,7 @@ pub struct Line;
 pub struct Triangle;
 
 impl Primitive for Point {
+    #[inline(always)]
     fn num_vertices() -> usize { 1 }
 
     fn create_ref_from_vertices<'p, K>(vertices: &'p [ClipVertex<K>]) -> PrimitiveRef<'p, K> {
@@ -86,6 +87,7 @@ impl Primitive for Point {
 }
 
 impl Primitive for Line {
+    #[inline(always)]
     fn num_vertices() -> usize { 2 }
 
     fn create_ref_from_vertices<'p, K>(vertices: &'p [ClipVertex<K>]) -> PrimitiveRef<'p, K> {
@@ -113,6 +115,7 @@ impl Primitive for Line {
 }
 
 impl Primitive for Triangle {
+    #[inline(always)]
     fn num_vertices() -> usize { 3 }
 
     fn create_ref_from_vertices<'p, K>(vertices: &'p [ClipVertex<K>]) -> PrimitiveRef<'p, K> {
