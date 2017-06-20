92b01b8bbe0e46908e3a303f30a3e76ff1b15469
Aaron Trent <novacrazy@gmail.com>
2017-06-20 04:27:36 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -51,19 +51,15 @@
 //! so memory allocation is done once per all meshes, but some more work needs to be done until I can plan that out in more detail.
 
 //#![deny(missing_docs)]
+#![allow(dead_code)]
 
+extern crate num_traits;
 extern crate nalgebra;
 extern crate alga;
 extern crate rayon;
-
-#[cfg(feature = "image_compat")]
-extern crate image;
+extern crate smallvec;
 
 pub mod utils;
-pub mod pixel;
-pub mod blend;
-pub mod texture;
-pub mod line;
 pub mod mesh;
 pub mod framebuffer;
 pub mod primitive;
@@ -72,17 +68,13 @@ pub mod interpolate;
 pub mod uniform;
 pub mod clip;
 pub mod pipeline;
-pub mod tuples;
 
-#[cfg(feature = "image_compat")]
-pub mod image_compat;
+//#[cfg(feature = "image_compat")]
+//pub mod image_compat;
 
-pub use pixel::Pixel;
-pub use blend::{GenericBlend, BoxedGenericBlend, Blend};
 pub use mesh::{Vertex, Mesh};
 pub use geometry::{FaceWinding, ClipVertex, ScreenVertex};
-pub use framebuffer::FrameBuffer;
+//pub use framebuffer::FrameBuffer;
 pub use interpolate::{Interpolate, barycentric_interpolate, linear_interpolate};
 pub use primitive::{Primitive, PrimitiveRef, PrimitiveMut};
-pub use pipeline::{Pipeline, VertexShader, FragmentShader, Fragment, PrimitiveStorage};
-
+//pub use pipeline::{Pipeline, VertexShader, FragmentShader, Fragment, PrimitiveStorage};
