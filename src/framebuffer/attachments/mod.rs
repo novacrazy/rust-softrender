c452093169fe8e17a69d95ba9b276db8ed047e1b
Aaron Trent <novacrazy@gmail.com>
2017-06-21 00:45:07 -0500

    Add EmptyAttachment trait

--- src/framebuffer/attachments/mod.rs
+++ src/framebuffer/attachments/mod.rs
@@ -10,6 +10,12 @@ pub use self::color::Color;
 pub use self::depth::Depth;
 pub use self::stencil::{Stencil, StencilOp, StencilTest, StencilType, StencilConfig, GenericStencilConfig, GenericStencil};
 
+/// Marker trait only defined for `()`, an empty tuple.
+pub trait EmptyAttachment {}
+
+impl EmptyAttachment for () {}
+
+/// Trait defining associated types of framebuffer attachments
 pub trait Attachments: Attachment {
     type Color: Attachment + Color;
     type Depth: Attachment + Depth;
