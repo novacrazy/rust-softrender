a758f084f74ea72f511133d656aab99d2deb76b7
Aaron Trent <novacrazy@gmail.com>
2017-06-23 03:14:58 -0500

    Add Texture precursors

--- src/texture.rs
+++ src/texture.rs
@@ -1,4 +1,8 @@
-use ::pixel::Pixel;
+use ::error::{RenderError, RenderResult};
+
+use ::color::Color;
+use ::pixel::{PixelBuffer, PixelRead, PixelWrite};
+use ::geometry::{Dimensions, Coordinate};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Filter {
@@ -6,23 +10,45 @@ pub enum Filter {
     Bilinear,
 }
 
+impl Default for Filter {
+    fn default() -> Filter {
+        Filter::Nearest
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
-pub enum Edge<P: Pixel> {
+pub enum Edge<C: Color> {
     /// Equivalent to `GL_CLAMP_TO_EDGE`
     Clamp,
     /// Equivalent to `GL_REPEAT`
     Wrap,
     /// Equivalent to `GL_CLAMP_TO_BORDER`
-    Border(P)
+    Border(C)
 }
 
-pub trait Texture<P: Pixel> {
-    fn width(&self) -> u32;
-    fn height(&self) -> u32;
+impl<C: Color> Default for Edge<C> {
+    fn default() -> Edge<C> { Edge::Clamp }
+}
 
-    fn pixel(&self, x: u32, y: u32) -> &P;
+pub trait Texture: PixelBuffer {}
 
-    fn sample(&self, x: f32, y: f32, filter: Filter) -> P {
+pub trait TextureRead: Texture + PixelRead {
+    /// Samples a pixel from a floating-point coordinate, applying the selected `Filter` and `Edge` behavior.
+    fn sample(&self, _x: f32, _y: f32, _filter: Filter, _edge: Edge<Self::Color>) -> RenderResult<Self::Color> {
         unimplemented!()
     }
 }
+
+pub trait TextureWrite: Texture + PixelWrite {
+    /// "Unsamples", or writes, to a floating-point coordinate, applying the selected `Filter` and `Edge` behavior.
+    ///
+    /// This allows writing to multiple pixels based on fractional coordinates.
+    fn unsample(&mut self, _x: f32, _y: f32, _filter: Filter, _edge: Edge<Self::Color>) -> RenderResult<()> {
+        unimplemented!()
+    }
+}
+
+//pub struct SliceTexture<'a, C: Color + 'a> {
+//    slice: &'a C,
+//    dimensions: Dimensions,
+//}
