12b28b5c852f405ccb07efbc8890b050cba235ee
Aaron Trent <novacrazy@gmail.com>
2017-06-24 14:03:00 -0500

    Cleanup and simplify dimensions

--- src/geometry/dimension.rs
+++ src/geometry/dimension.rs
@@ -6,6 +6,12 @@ use super::Coordinate;
 pub trait HasDimensions {
     /// Returns the dimensions of the object
     fn dimensions(&self) -> Dimensions;
+
+    /// Checks if the given coordinate is within the dimension bounds of the current object
+    #[inline]
+    fn in_bounds(&self, coord: Coordinate) -> bool {
+        self.dimensions().in_bounds(coord)
+    }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, PartialOrd)]
@@ -15,27 +21,20 @@ pub struct Dimensions {
 }
 
 impl Dimensions {
-    #[inline]
+    #[inline(always)]
     pub fn new(width: u32, height: u32) -> Dimensions {
         Dimensions { width, height }
     }
 
+    /// Returns the number of pixels as `usize` by multiplying the current width and height
     #[inline]
-    pub fn pixels(&self) -> usize {
+    pub fn area(&self) -> usize {
         self.width as usize * self.height as usize
     }
 
+    /// Checks if the given coordinate is within the dimension bounds
     #[inline]
-    pub fn valid(&self, coord: Coordinate) -> bool {
-        let Coordinate { x, y } = coord;
-
-        x < self.width && y < self.height
-    }
-
-    #[inline]
-    pub fn check_valid(&self, coord: Coordinate) -> RenderResult<()> {
-        if self.valid(coord) { Ok(()) } else {
-            throw!(RenderError::InvalidPixelCoordinate)
-        }
+    pub fn in_bounds(&self, coord: Coordinate) -> bool {
+        coord.x < self.width && coord.y < self.height
     }
 }
