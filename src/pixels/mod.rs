c2e7ec5a89bba258e509f7e06424b45854f71c06
Aaron Trent <novacrazy@gmail.com>
2017-06-25 05:53:33 -0500

    Generic letter change T -> P

--- src/pixels/mod.rs
+++ src/pixels/mod.rs
@@ -74,54 +74,54 @@ pub trait PixelWrite: PixelRead {
 /// Immutable reference to a pixel.
 ///
 /// Provides a read-only accessor for the pixel at the coordinates given at creation.
-pub struct PixelRef<'a, T: 'a>(pub ( in ::pixels) usize,
-                               pub ( in ::pixels) &'a T) where T: PixelRead;
+pub struct PixelRef<'a, P: 'a>(pub ( in ::pixels) usize,
+                               pub ( in ::pixels) &'a P) where P: PixelRead;
 
 /// Mutable reference to a pixel
 ///
 /// Provides a writable accessor for the pixel at the coordinates given at creation.
-pub struct PixelMut<'a, T: 'a>(pub ( in ::pixels) usize,
-                               pub ( in ::pixels) &'a mut T) where T: PixelWrite;
+pub struct PixelMut<'a, P: 'a>(pub ( in ::pixels) usize,
+                               pub ( in ::pixels) &'a mut P) where P: PixelWrite;
 
-impl<'a, T: 'a> PixelRef<'a, T> where T: PixelRead {
+impl<'a, P: 'a> PixelRef<'a, P> where P: PixelRead {
     #[inline(always)]
-    pub ( in ::pixels ) fn new(index: usize, framebuffer: &'a T) -> PixelRef<'a, T> {
+    pub ( in ::pixels ) fn new(index: usize, framebuffer: &'a P) -> PixelRef<'a, P> {
         PixelRef(index, framebuffer)
     }
 
     /// Get the pixel
     #[inline]
-    pub fn get(&self) -> <T as PixelBuffer>::Color {
+    pub fn get(&self) -> <P as PixelBuffer>::Color {
         unsafe { self.1.get_pixel_unchecked(self.0) }
     }
 }
 
-impl<'a, T: 'a> PixelMut<'a, T> where T: PixelWrite {
+impl<'a, P: 'a> PixelMut<'a, P> where P: PixelWrite {
     #[inline(always)]
-    pub ( in ::pixels ) fn new(index: usize, framebuffer: &'a mut T) -> PixelMut<'a, T> {
+    pub ( in ::pixels ) fn new(index: usize, framebuffer: &'a mut P) -> PixelMut<'a, P> {
         PixelMut(index, framebuffer)
     }
 
     /// Get the pixel
     #[inline]
-    pub fn get(&self) -> <T as PixelBuffer>::Color {
+    pub fn get(&self) -> <P as PixelBuffer>::Color {
         unsafe { self.1.get_pixel_unchecked(self.0) }
     }
 
     /// Set the pixel
     #[inline]
-    pub fn set(&mut self, color: <T as PixelBuffer>::Color) {
+    pub fn set(&mut self, color: <P as PixelBuffer>::Color) {
         unsafe { self.1.set_pixel_unchecked(self.0, color) }
     }
 
     /// Downcast the current `PixelMut` into an immutable `PixelRef`.
     #[inline]
-    pub fn into_ref(self) -> PixelRef<'a, T> {
+    pub fn into_ref(self) -> PixelRef<'a, P> {
         PixelRef(self.0, self.1)
     }
 }
 
-impl<'a, T: 'a> From<PixelMut<'a, T>> for PixelRef<'a, T> where T: PixelWrite {
+impl<'a, P: 'a> From<PixelMut<'a, P>> for PixelRef<'a, P> where P: PixelWrite {
     #[inline]
-    fn from(pixel: PixelMut<'a, T>) -> PixelRef<'a, T> { pixel.into_ref() }
+    fn from(pixel: PixelMut<'a, P>) -> PixelRef<'a, P> { pixel.into_ref() }
 }
