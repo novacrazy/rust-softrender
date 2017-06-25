6f46a9c174c1c802a49984d9750aca806706fc99
Aaron Trent <novacrazy@gmail.com>
2017-06-25 05:28:59 -0500

    Simplify and expand the functionality of the PixelBuffer traits

--- src/pixels/iterator.rs
+++ src/pixels/iterator.rs
@@ -1,3 +1,29 @@
-//pub struct Pixels<'a, C>;
+use super::{PixelBuffer, PixelRead, PixelWrite, PixelRef, PixelMut};
 
-//pub struct PixelsMut<'a, C>;
+pub struct PixelBufferIter<'a, P: 'a> where P: PixelRead {
+    pub ( in ::pixels) buffer: &'a P,
+    pub ( in ::pixels) position: usize,
+    pub ( in ::pixels) max_len: usize,
+}
+
+impl<'a, P: 'a> DoubleEndedIterator for PixelBufferIter<'a, P> where P: PixelRead {
+    fn next_back(&mut self) -> Option<PixelRef<'a, P>> {
+        if self.position == 0 { None } else {
+            let res = PixelRef(self.position, self.buffer);
+            self.position -= 1;
+            Some(res)
+        }
+    }
+}
+
+impl<'a, P: 'a> Iterator for PixelBufferIter<'a, P> where P: PixelRead {
+    type Item = PixelRef<'a, P>;
+
+    fn next(&mut self) -> Option<PixelRef<'a, P>> {
+        if self.position >= self.max_len { None } else {
+            let res = PixelRef(self.position, self.buffer);
+            self.position += 1;
+            Some(res)
+        }
+    }
+}
--- src/pixels/mod.rs
+++ src/pixels/mod.rs
@@ -1,4 +1,127 @@
-pub mod pixelbuffer;
+//! Pixel accessor structures
+use std::slice;
+
+use ::error::{RenderResult, RenderError};
+use ::color::Color;
+use ::geometry::{Dimensions, Coordinate, HasDimensions};
+
 pub mod iterator;
 
-pub use self::pixelbuffer::{PixelBuffer, PixelRead, PixelWrite, PixelRef, PixelMut};
+pub use self::iterator::PixelBufferIter;
+
+/// Generic buffer type trait, which defines the `Color` type for any pixel in the buffer
+pub trait PixelBuffer: Sized + HasDimensions {
+    type Color: Color;
+}
+
+
+/// Defines methods for reading raw pixel values.
+pub trait PixelRead: PixelBuffer {
+    /// Unsafely access a pixel at the given index without checking bounds.
+    ///
+    /// This is meant for internal use, do not attempt to use it directly. Please use
+    /// `pixel_ref` or `pixel_iter` to access pixel values safely.
+    unsafe fn get_pixel_unchecked(&self, index: usize) -> Self::Color;
+
+    /// Get a "reference" to the pixel at the given coordinate.
+    ///
+    /// Throws `RenderError::InvalidPixelCoordinate` on invalid pixel coordinates.
+    fn pixel_ref<'a>(&'a self, coord: Coordinate) -> RenderResult<PixelRef<'a, Self>> {
+        let dim = self.dimensions();
+
+        if dim.in_bounds(coord) {
+            Ok(PixelRef::new(coord.into_index(dim), self))
+        } else {
+            throw!(RenderError::InvalidPixelCoordinate);
+        }
+    }
+
+    /// Efficient iterator for accessing all pixels in the buffer.
+    ///
+    /// However, certain framebuffers or pixelbuffers may provide their
+    /// own iterators for even more efficient access.
+    fn pixel_iter<'a>(&'a self) -> PixelBufferIter<'a, Self> {
+        PixelBufferIter {
+            buffer: self,
+            position: 0,
+            max_len: self.dimensions().area()
+        }
+    }
+}
+
+/// Defines methods for writing to raw pixel values.
+pub trait PixelWrite: PixelRead {
+    /// Unsafely access a pixel at the given index without checking bounds.
+    ///
+    /// This is meant for internal use, do not attempt to use it directly. Please use
+    /// `pixel_mut` to access pixel values safely.
+    unsafe fn set_pixel_unchecked(&mut self, index: usize, color: Self::Color);
+
+    /// Get a mutable "reference" to the pixel at the given coordinate.
+    ///
+    /// Throws `RenderError::InvalidPixelCoordinate` on invalid pixel coordinates.
+    fn pixel_mut<'a>(&'a mut self, coord: Coordinate) -> RenderResult<PixelMut<'a, Self>> {
+        let dim = self.dimensions();
+
+        if dim.in_bounds(coord) {
+            Ok(PixelMut::new(coord.into_index(dim), self))
+        } else {
+            throw!(RenderError::InvalidPixelCoordinate);
+        }
+    }
+}
+
+/// Immutable reference to a pixel.
+///
+/// Provides a read-only accessor for the pixel at the coordinates given at creation.
+pub struct PixelRef<'a, T: 'a>(pub ( in ::pixels) usize,
+                               pub ( in ::pixels) &'a T) where T: PixelRead;
+
+/// Mutable reference to a pixel
+///
+/// Provides a writable accessor for the pixel at the coordinates given at creation.
+pub struct PixelMut<'a, T: 'a>(pub ( in ::pixels) usize,
+                               pub ( in ::pixels) &'a mut T) where T: PixelWrite;
+
+impl<'a, T: 'a> PixelRef<'a, T> where T: PixelRead {
+    #[inline(always)]
+    pub ( in ::pixels ) fn new(index: usize, framebuffer: &'a T) -> PixelRef<'a, T> {
+        PixelRef(index, framebuffer)
+    }
+
+    /// Get the pixel
+    #[inline]
+    pub fn get(&self) -> <T as PixelBuffer>::Color {
+        unsafe { self.1.get_pixel_unchecked(self.0) }
+    }
+}
+
+impl<'a, T: 'a> PixelMut<'a, T> where T: PixelWrite {
+    #[inline(always)]
+    pub ( in ::pixels ) fn new(index: usize, framebuffer: &'a mut T) -> PixelMut<'a, T> {
+        PixelMut(index, framebuffer)
+    }
+
+    /// Get the pixel
+    #[inline]
+    pub fn get(&self) -> <T as PixelBuffer>::Color {
+        unsafe { self.1.get_pixel_unchecked(self.0) }
+    }
+
+    /// Set the pixel
+    #[inline]
+    pub fn set(&mut self, color: <T as PixelBuffer>::Color) {
+        unsafe { self.1.set_pixel_unchecked(self.0, color) }
+    }
+
+    /// Downcast the current `PixelMut` into an immutable `PixelRef`.
+    #[inline]
+    pub fn into_ref(self) -> PixelRef<'a, T> {
+        PixelRef(self.0, self.1)
+    }
+}
+
+impl<'a, T: 'a> From<PixelMut<'a, T>> for PixelRef<'a, T> where T: PixelWrite {
+    #[inline]
+    fn from(pixel: PixelMut<'a, T>) -> PixelRef<'a, T> { pixel.into_ref() }
+}
