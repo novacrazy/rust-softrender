425bb9f4ce10f5d7d188bb8ddcd0b2394db9d0b4
Aaron Trent <novacrazy@gmail.com>
2017-06-30 11:49:02 -0500

    Move clip module to geometry module

--- src/geometry/clip.rs
+++ src/geometry/clip.rs
@@ -1,7 +1,10 @@
 //! Clipping planes
 
+use num_traits::Zero;
+
 use nalgebra::coordinates::XYZW;
 
+use ::numeric::FloatScalar;
 use ::geometry::ClipVertex;
 use ::interpolate::Interpolate;
 
@@ -28,7 +31,7 @@ pub const ALL_CLIPPING_PLANES: [ClippingPlane; 6] = [
 impl ClippingPlane {
     /// Check if the clipping plane has the given clip-space point inside of it
     #[inline]
-    pub fn has_inside<K>(self, v: &ClipVertex<K>) -> bool {
+    pub fn has_inside<N: FloatScalar, K>(self, v: &ClipVertex<N, K>) -> bool {
         let XYZW { x, y, z, w } = *v.position;
 
         match self {
@@ -36,14 +39,14 @@ impl ClippingPlane {
             ClippingPlane::Right => { x <= w }
             ClippingPlane::Top => { y >= -w }
             ClippingPlane::Bottom => { y <= w }
-            ClippingPlane::Near => { z >= 0.0 }
+            ClippingPlane::Near => { z >= Zero::zero() }
             ClippingPlane::Far => { z <= w }
         }
     }
 
     /// Find the intersection of a line and the clipping plane
     #[inline]
-    pub fn intersect<K>(self, v1: &ClipVertex<K>, v2: &ClipVertex<K>) -> ClipVertex<K> where K: Interpolate {
+    pub fn intersect<N: FloatScalar, K>(self, v1: &ClipVertex<N, K>, v2: &ClipVertex<N, K>) -> ClipVertex<N, K> where K: Interpolate {
         let XYZW { x: x1, y: y1, z: z1, w: w1 } = *v1.position;
         let XYZW { x: x2, y: y2, z: z2, w: w2 } = *v2.position;
 
--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -5,9 +5,11 @@ pub mod coordinate;
 pub mod winding;
 pub mod clipvertex;
 pub mod screenvertex;
+pub mod clip;
 
 pub use self::dimension::{Dimensions, HasDimensions};
 pub use self::coordinate::Coordinate;
 pub use self::winding::FaceWinding;
 pub use self::clipvertex::ClipVertex;
 pub use self::screenvertex::ScreenVertex;
+pub use self::clip::{ClippingPlane, ALL_CLIPPING_PLANES};
