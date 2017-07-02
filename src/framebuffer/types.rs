f5f0df4a3666207e9c51ead073dceea3c512482e
Aaron Trent <novacrazy@gmail.com>
2017-07-02 14:30:03 -0500

    Add StencilConfig type alias

--- src/framebuffer/types.rs
+++ src/framebuffer/types.rs
@@ -1,5 +1,7 @@
 use super::{Framebuffer, Attachments};
+use super::attachments::Stencil;
 
 pub type ColorAttachment<F> = <<F as Framebuffer>::Attachments as Attachments>::Color;
 pub type DepthAttachment<F> = <<F as Framebuffer>::Attachments as Attachments>::Depth;
-pub type StencilAttachment<F> = <<F as Framebuffer>::Attachments as Attachments>::Stencil;
+pub type StencilAttachment<F> = <<<F as Framebuffer>::Attachments as Attachments>::Stencil as Stencil>::Type;
+pub type StencilConfig<F> = <<<F as Framebuffer>::Attachments as Attachments>::Stencil as Stencil>::Config;
