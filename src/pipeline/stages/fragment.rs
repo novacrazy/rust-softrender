750a4563f268b14fe1a4b008a64740ad8e6867e8
Aaron Trent <novacrazy@gmail.com>
2017-07-12 19:23:16 -0500

    Implement the rest of the fragment shader and raserizer code

--- src/pipeline/stages/fragment.rs
+++ src/pipeline/stages/fragment.rs
@@ -4,13 +4,13 @@ use std::ops::{Deref, DerefMut};
 
 use rayon::prelude::*;
 
-use num_traits::{Float, One, Zero, NumCast};
+use num_traits::{Float, One, Zero, NumCast, cast};
 use nalgebra::coordinates::XYZW;
 
 use ::error::RenderResult;
 
 use ::numeric::utils::min;
-use ::color::Color;
+use ::color::{Color, ColorAlpha};
 use ::color::blend::Blend;
 use ::pixels::{PixelRead, PixelWrite};
 use ::framebuffer::{UnsafeFramebuffer, Framebuffer};
@@ -27,7 +27,7 @@ use ::pipeline::PipelineObject;
 use ::framebuffer::types::DepthAttachment;
 use ::pipeline::types::{PipelineUniforms, Pixel, StencilValue};
 
-pub const DEFAULT_TILE_SIZE: Dimensions = Dimensions { width: 32, height: 32 };
+pub const DEFAULT_TILE_SIZE: Dimensions = Dimensions { width: 128, height: 128 };
 
 /// Fragment shader stage.
 ///
@@ -84,11 +84,36 @@ impl<'a, P: 'a, V, T, K, B> FragmentShader<'a, P, V, T, K, B> where P: PipelineO
         self.cull_faces = cull;
     }
 
+    pub fn with_faces_culled(self, cull: Option<FaceWinding>) -> Self {
+        FragmentShader {
+            cull_faces: cull,
+            ..self
+        }
+    }
+
     /// Enables drawing antialiased lines for `Line` primitives
     /// primitives using Xiaolin Wu's algorithm,
     /// otherwise Bresenham's Algorithm is used.
     pub fn antialiased_lines(&mut self, enable: bool) {
         self.antialiased_lines = enable;
+    }
+
+    pub fn with_antialiased_lines(self, enable: bool) -> Self {
+        FragmentShader {
+            antialiased_lines: enable,
+            ..self
+        }
+    }
+
+    pub fn tile_size(&mut self, tile_size: Dimensions) {
+        self.tile_size = tile_size;
+    }
+
+    pub fn with_tile_size(self, tile_size: Dimensions) -> Self {
+        FragmentShader {
+            tile_size,
+            ..self
+        }
     }
 
     /// Duplicates all references to internal state to return a cloned fragment shader,
@@ -164,15 +189,18 @@ impl<'a, P: 'a, V, T, K, B> FragmentShader<'a, P, V, T, K, B> where P: PipelineO
         let tiles = {
             let mut tiles = Vec::new();
 
+            let xmax = dimensions.width - 1;
+            let ymax = dimensions.height - 1;
+
             let mut y = 0;
 
-            while y < dimensions.height {
+            while y < ymax {
                 let mut x = 0;
 
-                let next_y = min(y + tile_size.height, dimensions.height);
+                let next_y = min(y + tile_size.height, ymax);
 
-                while x < dimensions.width {
-                    let next_x = min(x + tile_size.width, dimensions.width);
+                while x < xmax {
+                    let next_x = min(x + tile_size.width, xmax);
 
                     tiles.push((
                         Coordinate::new(x, y),
@@ -203,10 +231,145 @@ impl<'a, P: 'a, V, T, K, B> FragmentShader<'a, P, V, T, K, B> where P: PipelineO
         let seriously_dont = NeverDoThis { pipeline: pipeline as *mut P };
 
         tiles.into_par_iter().for_each(|tile: (Coordinate, Coordinate)| {
-            println!("Tile {:?}", tile);
+            let bounds = ((cast(tile.0.x).unwrap(), cast(tile.0.y).unwrap()),
+                          (cast(tile.1.x).unwrap(), cast(tile.1.y).unwrap()));
 
             // Get the unsafe mutable reference to the pipeline
             let pipeline: &mut P = unsafe { &mut *seriously_dont.pipeline };
+
+            // Fetch stencil test and operation before tile loop
+            let stencil_test = pipeline.stencil_config().get_test();
+            let stencil_op = pipeline.stencil_config().get_op();
+
+            let (uniforms, framebuffer) = pipeline.uniforms_framebuffer_mut();
+
+            ////////////////////////////////////////
+            let rasterize_point = |framebuffer: &mut <P as PipelineObject>::Framebuffer,
+                                   p: &ScreenVertex<V::Scalar, K>| {
+                let XYZW { x, y, z, .. } = *p.position;
+
+                if (bounds.0).0 <= x && x < (bounds.1).0 && (bounds.0).1 <= y && y < (bounds.1).1 {
+                    let coord = Coordinate::new(cast(x).unwrap(), cast(y).unwrap());
+
+                    let index = coord.into_index(dimensions);
+
+                    // Get stencil buffer value for this pixel
+                    let framebuffer_stencil_value = unsafe { framebuffer.get_stencil_unchecked(index) };
+
+                    // perform stencil test
+                    if stencil_test.test(framebuffer_stencil_value, stencil_value) {
+                        // Calculate new stencil value
+                        let new_stencil_value = stencil_op.op(framebuffer_stencil_value, stencil_value);
+
+                        // Set stencil value for this pixel
+                        unsafe { framebuffer.set_stencil_unchecked(index, new_stencil_value); }
+
+                        if z < Zero::zero() {
+                            let d: DepthAttachment<P::Framebuffer> = Depth::from_scalar(z);
+
+                            let dt = unsafe { framebuffer.get_depth_unchecked(index) };
+
+                            // Check if point is in front of other geometry
+                            if d >= dt {
+                                // Perform fragment shading
+                                let fragment = fragment_shader(p, &uniforms);
+
+                                match fragment {
+                                    Fragment::Discard => (),
+                                    Fragment::Color(c) => {
+                                        let p = unsafe { framebuffer.get_pixel_unchecked(index) };
+
+                                        unsafe {
+                                            framebuffer.set_pixel_unchecked(index, blend.blend(c, p));
+                                            framebuffer.set_depth_unchecked(index, d);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            ////////////////////////////////////////
+            let rasterize_line = |framebuffer: &mut <P as PipelineObject>::Framebuffer,
+                                  start: &ScreenVertex<V::Scalar, K>,
+                                  end: &ScreenVertex<V::Scalar, K>| {
+                use ::geometry::line::liang_barsky_iterative;
+                use super::rasterization::line::{draw_line_bresenham, draw_line_xiaolin_wu};
+
+                let XYZW { x: x1, y: y1, .. } = *start.position;
+                let XYZW { x: x2, y: y2, .. } = *end.position;
+
+                if let Some(((x1, y1), (x2, y2))) = liang_barsky_iterative((x1, y1), (x2, y2), bounds) {
+                    let d = (x1 - x2).hypot(y1 - y2);
+
+                    let rasterize_fragment = |x: i64, y: i64, alpha: f64| {
+                        if x >= 0 && y >= 0 {
+                            let coord = Coordinate::new(x as u32, y as u32);
+
+                            let index = coord.into_index(dimensions);
+
+                            // Get stencil buffer value for this pixel
+                            let framebuffer_stencil_value = unsafe { framebuffer.get_stencil_unchecked(index) };
+
+                            // perform stencil test
+                            if stencil_test.test(framebuffer_stencil_value, stencil_value) {
+                                // Calculate new stencil value
+                                let new_stencil_value = stencil_op.op(framebuffer_stencil_value, stencil_value);
+
+                                // Set stencil value for this pixel
+                                unsafe { framebuffer.set_stencil_unchecked(index, new_stencil_value); }
+
+                                // Real screen position should be in the center of the pixel.
+                                let (xf, yf) = (cast::<_, V::Scalar>(x).unwrap() + one_half,
+                                                cast::<_, V::Scalar>(y).unwrap() + one_half);
+
+                                let t = (x1 - xf).hypot(y1 - yf) / d;
+
+                                let position = Interpolate::linear_interpolate(t, &start.position, &end.position);
+
+                                let z = position.z;
+
+                                if z < Zero::zero() {
+                                    let d: DepthAttachment<P::Framebuffer> = Depth::from_scalar(z);
+
+                                    let dt = unsafe { framebuffer.get_depth_unchecked(index) };
+
+                                    // Check if point is in front of other geometry
+                                    if d >= dt {
+                                        // Perform fragment shading
+                                        let fragment = fragment_shader(&ScreenVertex {
+                                            position,
+                                            uniforms: Interpolate::linear_interpolate(t, &start.uniforms, &end.uniforms)
+                                        }, &uniforms);
+
+                                        match fragment {
+                                            Fragment::Discard => (),
+                                            Fragment::Color(c) => {
+                                                let p = unsafe { framebuffer.get_pixel_unchecked(index) };
+
+                                                unsafe {
+                                                    framebuffer.set_pixel_unchecked(index, blend.blend(c.mul_alpha(ColorAlpha::from_scalar(alpha)), p));
+                                                    framebuffer.set_depth_unchecked(index, d);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    };
+
+                    if antialiased_lines {
+                        draw_line_xiaolin_wu(cast(x1).unwrap(), cast(y1).unwrap(),
+                                             cast(x2).unwrap(), cast(y2).unwrap(), rasterize_fragment);
+                    } else {
+                        draw_line_bresenham(cast(x1).unwrap(), cast(y1).unwrap(),
+                                            cast(x2).unwrap(), cast(y2).unwrap(), rasterize_fragment)
+                    }
+                }
+            };
 
             ////////////////////////////////////////
             let rasterize_triangle = |framebuffer: &mut <P as PipelineObject>::Framebuffer,
@@ -235,7 +398,7 @@ impl<'a, P: 'a, V, T, K, B> FragmentShader<'a, P, V, T, K, B> where P: PipelineO
                     ($value:expr, $min:expr, $max:expr) => {{
                         // Store expressions as temp variables to avoid multiple evaluation
                         let value = $value; let min = $min; let max = $max;
-                        if value < NumCast::from(min).unwrap() { min } else if value > NumCast::from(max).unwrap() { max } else { NumCast::from(value).unwrap() }
+                        if value < cast(min).unwrap() { min } else if value > cast(max).unwrap() { max } else { cast(value).unwrap() }
                     }}
                 }
 
@@ -245,17 +408,15 @@ impl<'a, P: 'a, V, T, K, B> FragmentShader<'a, P, V, T, K, B> where P: PipelineO
                 let max = Coordinate::new(clamp_as_int!(x1.max(x2).max(x3), tile.0.x, tile.1.x),
                                           clamp_as_int!(y1.max(y2).max(y3), tile.0.y, tile.1.y));
 
-                // Fetch stencil test and operation before tile loop
-                let stencil_test = pipeline.stencil_config().get_test();
-                let stencil_op = pipeline.stencil_config().get_op();
-
                 let mut pixel = min;
 
-                while pixel.y < max.y {
+                while pixel.y <= max.y {
                     pixel.x = min.x;
 
-                    while pixel.x < max.x {
+                    while pixel.x <= max.x {
                         let index = pixel.into_index(dimensions);
+
+                        assert!(index < dimensions.area());
 
                         // Get stencil buffer value for this pixel
                         let framebuffer_stencil_value = unsafe { framebuffer.get_stencil_unchecked(index) };
@@ -271,8 +432,8 @@ impl<'a, P: 'a, V, T, K, B> FragmentShader<'a, P, V, T, K, B> where P: PipelineO
                             //continue on to fragment shading
 
                             // Real screen position should be in the center of the pixel.
-                            let (x, y) = (<V::Scalar as NumCast>::from(pixel.x).unwrap() + one_half,
-                                          <V::Scalar as NumCast>::from(pixel.y).unwrap() + one_half);
+                            let (x, y) = (cast::<_, V::Scalar>(pixel.x).unwrap() + one_half,
+                                          cast::<_, V::Scalar>(pixel.y).unwrap() + one_half);
 
                             // calculate barycentric coordinates of the current point
                             let u = ((y2 - y3) * (x - x3) + (x3 - x2) * (y - y3)) / det;
@@ -294,20 +455,22 @@ impl<'a, P: 'a, V, T, K, B> FragmentShader<'a, P, V, T, K, B> where P: PipelineO
                                     let dt = unsafe { framebuffer.get_depth_unchecked(index) };
 
                                     // Check if point is in front of other geometry
-                                    if d < dt {
+                                    if d >= dt {
                                         // Perform fragment shading
                                         let fragment = fragment_shader(&ScreenVertex {
                                             position,
                                             uniforms: Interpolate::barycentric_interpolate(u, &a.uniforms,
                                                                                            v, &b.uniforms,
                                                                                            w, &c.uniforms),
-                                        }, pipeline.uniforms());
+                                        }, uniforms);
 
                                         match fragment {
                                             Fragment::Discard => (),
                                             Fragment::Color(c) => {
+                                                let p = unsafe { framebuffer.get_pixel_unchecked(index) };
+
                                                 unsafe {
-                                                    framebuffer.set_pixel_unchecked(pixel.into_index(dimensions), c);
+                                                    framebuffer.set_pixel_unchecked(index, blend.blend(c, p));
                                                     framebuffer.set_depth_unchecked(index, d);
                                                 }
                                             }
@@ -324,7 +487,50 @@ impl<'a, P: 'a, V, T, K, B> FragmentShader<'a, P, V, T, K, B> where P: PipelineO
                 }
             };
 
-            //render
+            if T::is_triangle() {
+                if let Some(ref indexed_vertices) = *indexed_vertices {
+                    for triangle in mesh.indices.chunks(3) {
+                        let a = &indexed_vertices[triangle[0]];
+                        let b = &indexed_vertices[triangle[1]];
+                        let c = &indexed_vertices[triangle[2]];
+
+                        rasterize_triangle(framebuffer, a, b, c);
+                    }
+                }
+            }
+
+            for triangle in generated_primitives.tris.chunks(3) {
+                rasterize_triangle(framebuffer, &triangle[0], &triangle[1], &triangle[2]);
+            }
+
+            if T::is_line() {
+                if let Some(ref indexed_vertices) = *indexed_vertices {
+                    for line in mesh.indices.chunks(2) {
+                        let start = &indexed_vertices[line[0]];
+                        let end = &indexed_vertices[line[1]];
+
+                        rasterize_line(framebuffer, start, end);
+                    }
+                }
+            }
+
+            for line in generated_primitives.lines.chunks(2) {
+                rasterize_line(framebuffer, &line[0], &line[1]);
+            }
+
+            if T::is_point() {
+                if let Some(ref indexed_vertices) = *indexed_vertices {
+                    for index in &mesh.indices {
+                        let point = &indexed_vertices[*index];
+
+                        rasterize_point(framebuffer, point);
+                    }
+                }
+            }
+
+            for point in &generated_primitives.points {
+                rasterize_point(framebuffer, point);
+            }
         });
     }
 }
